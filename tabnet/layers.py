fa2af994d68ce5d8f2cc3c9191d2e097e8f174b8
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-11-04 09:15:51 +0100

    Remove GLU useless attribute n_units

--- tabnet/layers.py
+++ tabnet/layers.py
@@ -3,14 +3,10 @@ import tensorflow as tf
 
 class GLU(tf.keras.layers.Layer):
     """Gated Linear Unit activation function."""
-    def __init__(self, n_units):
-        super().__init__()
-        self.n_units = n_units
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def call(self, inputs):
-        return inputs[:, :self.n_units] * tf.keras.activations.sigmoid(inputs[:, self.n_units:])
+        half = inputs.shape[1] // 2
+        return inputs[:, :half] * tf.keras.activations.sigmoid(inputs[:, half:])
 
-    def get_config(self):
-        config = {'n_units': self.n_units}
-        base_config = super().get_config()
-        return {**base_config, **config}
